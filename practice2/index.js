1dc21debf46cdf9e7990c237022fd9e41b9a811a
Usman7686 <shahusmanali78@gmail.com>
2025-11-09 12:39:26 -0800

    add.practice.index.js,index.html

---
+++ practice2/index.js
@@ -0,0 +1,6 @@
+function showmessage() {
+    var name = prompt("what is your name");
+var age = prompt("what is your age");
+var city = prompt("where do you live");
+document.getElementById("message").
+innerText = `Hey ${name}, Your are ${age} old , you live in ${city}, wellcome in website`}
